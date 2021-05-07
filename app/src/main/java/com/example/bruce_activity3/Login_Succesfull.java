f636b41badd811be1b8cf64078750a862710aba4
FrancisSeanBruce <francissean.bruce@perpetual.edu.ph>
2021-05-08 03:18:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bruce_activity3/Login_Succesfull.java
@@ -0,0 +1,14 @@
+package com.example.bruce_activity3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Succesfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__succesfull);
+    }
+}
